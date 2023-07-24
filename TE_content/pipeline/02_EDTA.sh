7099602a0153e178cfbb4b358ff4bc6de3ad97fa
Mark Yacoub <68869747+myacoub005@users.noreply.github.com>
2023-07-24 09:25:03 -0700

    Create 02_EDTA.sh

---
+++ TE_content/pipeline/02_EDTA.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH -p batch -N 1 -n 48 --mem 96gb --out logs/EDTA.%a.log
+
+module load edta
+
+if [[ -z ${SLURM_CPUS_ON_NODE} ]]; then
+    CPUS=1
+else
+    CPUS=${SLURM_CPUS_ON_NODE}
+fi
+
+N=${SLURM_ARRAY_TASK_ID}
+
+if [ ! $N ]; then
+    N=$1
+    if [ ! $N ]; then
+        echo "need to provide a number by --array or cmdline"
+        exit
+    fi
+fi
+
+IFS=,
+tail -n +2 samples_NP.tsv | sed -n ${N}p | while read SAMPLE FQ
+do
+
+INDIR=genome
+NAME=$INDIR/$SAMPLE.scaffolds.fa
+PREFIX=$(basename $NAME .scaffolds.fa)
+
+EDTA.pl --genome $INDIR/$SAMPLE.scaffolds.fa --sensitive 1 --force 1
+done
