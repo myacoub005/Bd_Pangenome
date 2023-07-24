1c2d4f14b0dbb148c5a5fd9869da62961dd6b923
Mark Yacoub <68869747+myacoub005@users.noreply.github.com>
2023-07-24 09:21:52 -0700

    Create 00_download.sh

---
+++ TE_content/00_download.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#SBATCH -p short --out logs/download.log
+#curl -L -O https://ftp.ncbi.nlm.nih.gov/genomes/refseq/fungi/Batrachochytrium_dendrobatidis/latest_assembly_versions/GCF_000203795.1_v1.0/GCF_000203795.1_v1.0_genomic.gbff.gz
+
+DB=genome
+mkdir -p $DB
+URL=https://ftp.ncbi.nlm.nih.gov/genomes/genbank/fungi/Batrachochytrium_dendrobatidis/latest_assembly_versions/GCA_000149865.1_BD_JEL423/
+for n in GCA_000149865.1_BD_JEL423_genomic.gtf GCA_000149865.1_BD_JEL423_genomic.fna
+
+do
+    if [ ! -s $DB/$n ]; then # if the file doesn't exist download and uncompress
+	curl -L $URL/$n.gz | pigz -dc > $DB/$n
+    fi
+done
